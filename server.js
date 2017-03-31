ffad75a88e269ce476553e155f6498a9af6d2734
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-04-01 00:42:45 +1300

    Cleanup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import chalk from 'chalk';
 
 const port = 3000;
 const app = express();
@@ -12,10 +13,13 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
+console.log(chalk.green('Starting app in dev mode...'));
+
 app.listen(port, function(err){
     if(err){
         console.log(err);
     }else{
         open('http://localhost:' + port);
+        console.log(chalk.green('Server started on http://localhost:' + port));
     }
 });
--- start-message.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by keilc on 31/03/2017.
- */
-
-import chalk from 'chalk';
-
-console.log(chalk.green('Starting app in dev mode...'));
