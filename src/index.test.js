f7bba0ba80c2258fb64e033776379c313cba8d69
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-04-06 19:24:23 +1200

    Initial configuration test using Mocha

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by keilc on 6/04/2017.
+ */
+
+import { expect } from 'chai';
+
+describe('Tester for test asserts', () => {
+    it('should pass', () => {
+        expect(true).to.equal(true);
+})
+});
