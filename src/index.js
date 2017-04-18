3b7856e18c59c067528c6dd90a8c7715d8967a73
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-04-18 22:51:40 +1200

    Populate table of users via API call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 /**
  * Created by keilc on 1/04/2017.
  */
+import {getUsers} from './api/user.api';
 
-console.log('this is a test for webpack bundler'); // eslint-disable-line no-console
+// Populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody += `<tr>
+          <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+          <td>${user.id}</td>
+          <td>${user.firstName}</td>
+          <td>${user.lastName}</td>
+          <td>${user.email}</td>
+          </tr>`
+    });
+    global.document.getElementById('users').innerHTML = usersBody;
+});
